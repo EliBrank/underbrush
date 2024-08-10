da1b45dcfa1a483234c3522d55af4f391c23fe55
DaveyCHaysIII <63436710+DaveyCHaysIII@users.noreply.github.com>
2024-08-10 11:15:39 -0500

    fixed next_level scene, tweaked lvl 1

--- A_fairy_tale/Next_Level.gd
+++ A_fairy_tale/Next_Level.gd
@@ -5,9 +5,9 @@ func _on_body_entered(body):
 	print("Level complete!")
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
-	timer.start()
-	
-func _on_timer_timeout():
+	timer.start()	
+
+func _on_level_timer_timeout():
 	print("timer ended. Reloading world:")
 	Engine.time_scale = 1
 	get_tree().reload_current_scene()

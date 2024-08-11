6ad6d89b574970751238bf80e262ef6c8779b666
EliBrank <eliasbrinkman@gmail.com>
2024-08-11 11:59:56 -0500

    Make more tweaks to Level 3 design, implement level switching script

--- A_fairy_tale/scripts/Next_Level.gd
+++ A_fairy_tale/scripts/Next_Level.gd
@@ -1,13 +1,20 @@
 extends Area2D
+
 @onready var timer = $Level_timer
+
+@export var next_level: String
 
 func _on_body_entered(body):
 	print("Level complete!")
-	Engine.time_scale = 0.5
-	body.get_node("CollisionShape2D").queue_free()
+	Engine.time_scale = 0.25
 	timer.start()
 
 func _on_level_timer_timeout():
-	print("timer ended. Reloading world:")
+	print("timer ended. Loading next level...")
 	Engine.time_scale = 1
-	get_tree().reload_current_scene()
+	
+	if next_level != "":
+		get_tree().change_scene_to_file(next_level)
+	else:
+		print("no next scene set... reloading current level")
+		get_tree().reload_current_scene()
